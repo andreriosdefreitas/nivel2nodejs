6445c696b8687aae7d0f40271ddb1437d21e8db5
André Rios <andre.rios.freitas@gmail.com>
2020-09-01 08:32:15 +0100

    GoBarber - exception handling

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -10,22 +10,18 @@ const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthtenticated);
 
 appointmentsRouter.post('/', async (request, response) => {
-    try {
-        const { provider_id, date } = request.body;
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppointmentService = new CreateAppointmentService();
+    const createAppointmentService = new CreateAppointmentService();
 
-        const appointment = await createAppointmentService.execute({
-            provider_id,
-            date: parsedDate,
-        });
+    const appointment = await createAppointmentService.execute({
+        provider_id,
+        date: parsedDate,
+    });
 
-        return response.json(appointment);
-    } catch (err) {
-        return response.status(err.statusCode).json({ error: err.message });
-    }
+    return response.json(appointment);
 });
 
 appointmentsRouter.get('/', async (request, response) => {
--- primeiro-projeto-node/src/routes/users.routes.ts
+++ primeiro-projeto-node/src/routes/users.routes.ts
@@ -9,15 +9,11 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
-        const createUserService = new CreateUserService();
-        const user = await createUserService.execute({ name, email, password });
-        delete user.password;
-        return response.json(user);
-    } catch (err) {
-        return response.status(err.statusCode).json({ error: err.message });
-    }
+    const { name, email, password } = request.body;
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({ name, email, password });
+    delete user.password;
+    return response.json(user);
 });
 
 usersRouter.patch(
@@ -25,17 +21,13 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateUserAvatarService = new UpdateUserAvatarService();
-            const user = await updateUserAvatarService.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename,
-            });
-            delete user.password;
-            return response.json(user);
-        } catch (err) {
-            return response.status(err.statusCode).json({ error: err.message });
-        }
+        const updateUserAvatarService = new UpdateUserAvatarService();
+        const user = await updateUserAvatarService.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+        delete user.password;
+        return response.json(user);
     },
 );
 
