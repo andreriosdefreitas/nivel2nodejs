ffaa6dc5a55ec19556affeef26192a8fb88dc676
André Rios <andre.rios.freitas@gmail.com>
2020-09-21 08:33:41 +0100

    GoBarber BE - test coverage user

---
+++ primeiro-projeto-node/src/modules/users/repositories/FakeUsersRepository.ts
@@ -0,0 +1,33 @@
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { uuid } from 'uuidv4';
+import User from '../infra/typeorm/entities/User';
+
+class FakeUserRepository implements IUsersRepository {
+    private users: User[] = [];
+
+    public async findById(id: string): Promise<User | undefined> {
+        const user = this.users.find(u => u.id === id);
+        return user;
+    }
+
+    public async findByEmail(email: string): Promise<User | undefined> {
+        const user = this.users.find(u => u.email === email);
+        return user;
+    }
+
+    public async create(data: ICreateUserDTO): Promise<User> {
+        const user = new User();
+        Object.assign(user, { id: uuid() }, data);
+        this.users.push(user);
+        return user;
+    }
+
+    public async save(user: User): Promise<User> {
+        const findIndex = this.users.findIndex(u => u.id === user.id);
+        this.users[findIndex] = user;
+        return user;
+    }
+}
+
+export default FakeUserRepository;
---
+++ primeiro-projeto-node/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '@modules/users/repositories/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+    it('should be able to create a new user', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const createUser = new CreateUserService(fakeUsersRepository);
+        const user = await createUser.execute({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+        expect(user).toHaveProperty('id');
+    });
+
+    it('should not be able to create a new user with same email another', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const createUser = new CreateUserService(fakeUsersRepository);
+        const user = await createUser.execute({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+        expect(user).toHaveProperty('id');
+        expect(
+            createUser.execute({
+                name: 'Joao Silva',
+                email: 'joao@test.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
