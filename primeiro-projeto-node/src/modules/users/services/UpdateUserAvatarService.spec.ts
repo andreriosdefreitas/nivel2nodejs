a185bdaf89ceb99c5bc45fd3ab50c4f7b13307df
André Rios <andre.rios.freitas@gmail.com>
2020-09-22 21:19:58 +0100

    GoBarber BE - update user avatar

---
+++ primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,77 @@
+import AppError from '@shared/errors/AppError';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUserRepository from '../repositories/FakeUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+    it('should be able to update the user avatar', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar.jpg',
+        });
+
+        expect(user.avatar).toBe('avatar.jpg');
+    });
+
+    it('should not be able to update the user avatar with non exist user', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        expect(
+            updateUserAvatar.execute({
+                user_id: 'null',
+                avatarFilename: 'avatar.jpg',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should deleete old avatar when updating new one', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+        const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+        const updateUserAvatar = new UpdateUserAvatarService(
+            fakeUsersRepository,
+            fakeStorageProvider,
+        );
+
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar.jpg',
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFilename: 'avatar2.jpg',
+        });
+
+        expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+
+        expect(user.avatar).toBe('avatar2.jpg');
+    });
+});
