12b51eba47865fc79c09193126d89ff2a12381d5
André Rios <andre.rios.freitas@gmail.com>
2020-09-26 16:23:39 +0100

    GoBarber BE - update profile service

---
+++ primeiro-projeto-node/src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,112 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '../repositories/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUserRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfileService: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUserRepository();
+        fakeHashProvider = new FakeHashProvider();
+
+        updateProfileService = new UpdateProfileService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+    });
+
+    it('should be able to update the profile', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfileService.execute({
+            user_id: user.id,
+            name: 'Joao Silva Sauro',
+            email: 'joaosauro@test.com',
+        });
+
+        expect(updatedUser.name).toBe('Joao Silva Sauro');
+        expect(updatedUser.email).toBe('joaosauro@test.com');
+    });
+
+    it('should be able to update the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfileService.execute({
+            user_id: user.id,
+            name: 'Joao Silva Sauro',
+            email: 'joaosauro@test.com',
+            old_password: '123456',
+            password: '123123',
+        });
+
+        expect(updatedUser.password).toBe('123123');
+    });
+
+    it('should not be able to update the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                user_id: user.id,
+                name: 'Joao Silva Sauro',
+                email: 'joaosauro@test.com',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to update the password with wrong old password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                user_id: user.id,
+                name: 'Joao Silva Sauro',
+                email: 'joaosauro@test.com',
+                old_password: 'wrongOldPassword',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to change to another user email', async () => {
+        await fakeUsersRepository.create({
+            name: 'Joao Silva',
+            email: 'joao@test.com',
+            password: '123456',
+        });
+
+        const user = await fakeUsersRepository.create({
+            name: 'Test',
+            email: 'test@test.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                user_id: user.id,
+                name: 'Joao Silva',
+                email: 'joao@test.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
