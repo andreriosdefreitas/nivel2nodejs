2456053ec3eeb32607beb8eba197e8bb1528fd2c
André Rios <andre.rios.freitas@gmail.com>
2020-09-29 08:16:23 +0100

    GoBarber BE - appointments avaliability

--- primeiro-projeto-node/dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: 'cbe3637c7b4c588ff61fb1c4292cd638',
-        expiresIn: '1d',
-    },
-};
