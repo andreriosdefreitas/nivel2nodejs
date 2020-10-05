95de23ddebe906a8263543ccc132473fdf4ef57d
André Rios <andre.rios.freitas@gmail.com>
2020-10-05 09:50:37 +0100

    GoBarber BE - list providers cache

--- primeiro-projeto-node/src/modules/appointments/services/ListProvidersService.ts
+++ primeiro-projeto-node/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     user_id: string;
@@ -12,15 +13,21 @@ class ListProvidersService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        const users = await this.usersRepository.findAllProviders({
-            except_user_id: user_id,
-        });
+        const cacheKey = `providers-list:${user_id}`;
+        let users = await this.cacheProvider.recover<User[]>(cacheKey);
 
-        if (users.length === 0) {
-            throw new AppError('User not found');
+        if (!users) {
+            users = await this.usersRepository.findAllProviders({
+                except_user_id: user_id,
+            });
+
+            await this.cacheProvider.save(cacheKey, JSON.stringify(users));
         }
 
         return users;
